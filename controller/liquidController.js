2b91bcc3868fcbeae4b07ff7bbbd0d536c383577
Jose Ruano <jruanogalvez@gmail.com>
2018-02-14 00:23:24 +0100

    Corregidos errores sobre los códigos HTTP y respuestas JSON.
    	modificado:    controller/liquidController.js

--- controller/liquidController.js
+++ controller/liquidController.js
@@ -20,7 +20,9 @@ exports.createLiquid = function(req, res) {
     liquid.save(function(err, data) {
         if(err) res.send(err);
         
-        res.status(301).json("Liquid added successfully " + liquid);
+        res.status(200).json({
+            "message": "Liquid added successfully"
+        });
     });
 };
 
@@ -28,7 +30,7 @@ exports.updateLiquid = function(req, res) {
     Liquids.update({_id: req.params.id, user_id: req.headers.user_id},
                    req.body, {upsert: true, new: true}, function(err, data) {
         if(err) throw err;
-        res.status(301).json("Liquid updated successfully ID: " + req.params.id);
+        res.status(200).json("Liquid updated successfully ID: " + req.params.id);
     });
 }
 
@@ -36,6 +38,6 @@ exports.deleteLiquid = function(req, res) {
     Liquids.remove({_id: req.params.id, user_id: req.headers.user_id}, function(err, data) {
         if(err) throw err;
         
-        res.status(301).json("Liquid deleted successfully. ID: " + req.params.id);
+        res.status(200).json("Liquid deleted successfully. ID: " + req.params.id);
     });
 };
