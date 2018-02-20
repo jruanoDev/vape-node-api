2421907ab5cfafea7d2a70d92e833af0b27de893
Jose Ruano <jruanogalvez@gmail.com>
2018-02-20 10:43:38 +0100

    Cambiado puerto al 5000
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express    = require('express'),
     app        = express(),
-    port       = process.env.PORT || 8080,
+    port       = process.env.PORT || 5000,
     mongoose   = require('mongoose'),
     Liquid     = require('./model/liquidModel'),
     Flavour    = require('./model/flavourModel'),
