c3141138ec531491b4ab5a52b7eb2231fc82d317
Jose Ruano <jruanogalvez@gmail.com>
2018-03-05 19:36:56 +0100

    modified:   server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET", "PUT", "POST", "DELETE", "OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, user_id");
     next();
 });
