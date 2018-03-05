f019c2491f097f4e9bed04eb83c85a7f50f74805
ocaparrostortosa <ocaparrostortosa@gmail.com>
2018-03-05 18:33:45 +0000

    modified:   server.js

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, user_id");
+    next();
 });
 
 var routes = require('./routes/routes');
